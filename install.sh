a95f49b294f71a46a09ca82f374a6b000a2bc76e
xiaohu <xiaohu.lv@fit2cloud.com>
2020-07-23 09:56:14 +0800

    优化

--- install.sh
+++ install.sh
@@ -23,5 +23,5 @@ echo "开始启动 KubeOperator"
 cd /opt/installer/kubeoperator/ && docker-compose up -d
 if [ $? = 0 ];then
 echo -e "======================= KubeOperator 安装完成 =======================\n"
-echo -e "请通过以下方式访问:\n URL: http://LOCAL_IP\n 用户名: admin\n 初始密码: kubeoperator@admin123"
+echo -e "请通过以下方式访问:\n URL: \033[33m http://LOCAL_IP \033[0m \n 用户名: \033[32m admin \033[0m \n 初始密码: \033[32m kubeoperator@admin123 \033[0m"
 fi
--- quick_start.sh
+++ quick_start.sh
@@ -3,7 +3,7 @@
 #  定义离线文件下载地址
 nexus_download_url="http://172.16.10.63/ko-3.0/data/nexus/nexus-data.origin.tar.gz"
 ansible_download_url="http://172.16.10.63/ko-3.0/data/ansible/ansible.tar.gz"
-kubeoperator_download_url="http://172.16.10.63/ko-3.0/data/install/kubeoperator_installer.tgz"
+kubeoperator_download_url="http://172.16.10.63/ko-3.0/data/install/kubeoperator_installer.tar.gz"
 
 # 判断 wget 命令是否安装
 if which wget;then
@@ -17,10 +17,10 @@ wget --no-check-certificate $ansible_download_url -c /root 2&> /dev/null
 wget --no-check-certificate $kubeoperator_download_url -c /root 2&> /dev/null
 
 # 解压离线文件
-tar zxvf /root/kubeoperator_installer.tgz -C /opt/
+tar zxvf /root/kubeoperator_installer.tar.gz -C /opt/
 if [ $? = 0 ];then
-  tar zxvf /root/nexus-data.origin.tar.gz -C /opt/installer/kubeoperator/data/ 2&> /dev/null
-  tar zxvf /root/ansible.tar.gz -C /opt/installer/kubeoperator/data/kobe/project/ 2&> /dev/null
+  tar zxvf /root/nexus-data.origin.tar.gz -C /opt/installer/kubeoperator/data/
+  tar zxvf /root/ansible.tar.gz -C /opt/installer/kubeoperator/data/kobe/project/
   mv /opt/installer/kubeoperator/data/kobe/project/ansible /opt/installer/kubeoperator/data/kobe/project/ko
 fi
 
