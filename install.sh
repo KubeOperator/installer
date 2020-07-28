728cd3c94048d2a4166ed96422be5dee9ebcab25
xiaohu <xiaohu.lv@fit2cloud.com>
2020-07-28 10:38:54 +0800

    初始化

--- install.sh
+++ install.sh
@@ -1,5 +1,31 @@
 #Install Latest Stable KubeOperator Release
+BASE_DIR=$(cd "$(dirname "$0")";pwd)
+PROJECT_DIR=$(dirname ${BASE_DIR})
+CURRENT_DIR=$(cd "$(dirname "$0")";pwd)
 
+if read -t 60 -p "设置KubeOperator安装目录,默认： /opt:" KO_BASE;then
+  echo "你选择的安装路径为 $KO_BASE"
+else
+  echo "设置超时，使用默认安装路径 /opt"
+fi
+
+function log() {
+   message="[KubeOperator Log]: $1 "
+   echo -e "${message}" 2>&1 | tee -a ${CURRENT_DIR}/install.log
+}
+
+# 解压离线文件
+if [ -f $CURRENT_DIR/kubeoperator_installer.tar.gz ];then
+# 执行在线安装
+  tar zxvf $CURRENT_DIR/kubeoperator_installer.tar.gz -C $CURRENT_DIR
+  tar zxvf $CURRENT_DIR/ansible.tar.gz -C $CURRENT_DIR
+  cp -rp $CURRENT_DIR/installer/kubeoperator $KO_BASE/
+  if [ $? = 0 ];then
+    tar zxvf $CURRENT_DIR/nexus-data.origin.tar.gz -C $KO_BASE/kubeoperator/data/
+    cp -rp $CURRENT_DIR/installer/kubeoperator/data/kobe/project/ansible $KO_BASE/kubeoperator/data/kobe/project/ko
+  fi
+# 执行离线安装
+fi
 
 # 1.检测 docker 是否存在
 if which docker docker-compose ;then
@@ -10,17 +36,29 @@ if which docker docker-compose ;then
     echo "docker 已经安装，跳过安装步骤"
   fi
 else
-  mkdir -p /etc/yum.repos.d /bak
-  mv /etc/yum.repos.d/*.repo /etc/yum.repos.d/bak
-  wget -O /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo 2&> /dev/null
-  wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo 2&> /dev/null
-  yum install docker  docker-compose -y
-  systemctl start docker
+   if [[ -d docker ]]; then
+      log "... 离线安装 docker"
+      cp docker/bin/* /usr/bin/
+      cp docker/service/docker.service /etc/systemd/system/
+      chmod +x /usr/bin/docker*
+      chmod 754 /etc/systemd/system/docker.service
+      log "... 启动 docker"
+      service docker start 2>&1 | tee -a ${CURRENT_DIR}/install.log
+   else
+      log "... 在线安装 docker"
+      curl -fsSL https://get.docker.com -o get-docker.sh 2>&1 | tee -a ${CURRENT_DIR}/install.log
+      sudo sh get-docker.sh 2>&1 | tee -a ${CURRENT_DIR}/install.log
+      log "... 在线安装 docker-compose"
+      sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+      sudo chmod +x /usr/local/bin/docker-compose
+      log "... 启动 docker"
+      service docker start 2>&1 | tee -a ${CURRENT_DIR}/install.log
+   fi
 fi
 
 #  2.启动 kubeoperator
 echo "开始启动 KubeOperator"
-cd /opt/installer/kubeoperator/ && docker-compose up -d
+cd  $KO_BASEkubeoperator/ && docker-compose up -d
 if [ $? = 0 ];then
 echo -e "======================= KubeOperator 安装完成 =======================\n"
 echo -e "请通过以下方式访问:\n URL: \033[33m http://LOCAL_IP \033[0m \n 用户名: \033[32m admin \033[0m \n 初始密码: \033[32m kubeoperator@admin123 \033[0m"
--- quick_start.sh
+++ quick_start.sh
@@ -1,28 +1,33 @@
 #Install Latest Stable KubeOperator Release
 
 #  定义离线文件下载地址
+export CURRENT_DIR=$(cd "$(dirname "$0")";pwd)
+export KOVERSION=$(curl -s https://github.com/metersphere/metersphere/releases/latest/download 2>&1 |grep -Eo "v([0-9]{1,}\.)+[0-9]{1,}")
+export ANSIBLE_VERSION=$(curl -s https://github.com/KubeOperator/ansible/releases/latest/download 2>&1 |grep -Eo "v([0-9]{1,}\.)+[0-9]{1,}")
+
 nexus_download_url="http://172.16.10.63/ko-3.0/data/nexus/nexus-data.origin.tar.gz"
 ansible_download_url="http://172.16.10.63/ko-3.0/data/ansible/ansible.tar.gz"
 kubeoperator_download_url="http://172.16.10.63/ko-3.0/data/install/kubeoperator_installer.tar.gz"
+#ansible_download_url="https://github.com/KubeOperator/ansible/releases/latest/download/"
+#kubeoperator_download_url="https://github.com/KubeOperator/KubeOperator/releases/latest/download/"
+
 
 # 判断 wget 命令是否安装
 if which wget;then
   echo "wget 已经安装"
-  else
+else
+  echo "wget 未安装，即将安装 wget"
   yum install wget -y
+  if [ $? = 0 ];then
+    echo "wget 安装成功"
+  else
+    echo "wget 安装失败，请手动安装"
 fi
 
-wget --no-check-certificate $nexus_download_url  2&> /dev/null
-wget --no-check-certificate $ansible_download_url  2&> /dev/null
-wget --no-check-certificate $kubeoperator_download_url  2&> /dev/null
-
-# 解压离线文件
-tar zxvf kubeoperator_installer.tar.gz -C /opt/
-if [ $? = 0 ];then
-  tar zxvf nexus-data.origin.tar.gz -C /opt/installer/kubeoperator/data/
-  tar zxvf ansible.tar.gz -C /opt/installer/kubeoperator/data/kobe/project/
-  mv /opt/installer/kubeoperator/data/kobe/project/ansible /opt/installer/kubeoperator/data/kobe/project/ko
-fi
+# 下载离线包
+wget --no-check-certificate $nexus_download_url
+wget --no-check-certificate $ansible_download_url
+wget --no-check-certificate $kubeoperator_download_url
 
 cd /opt/installer/
 /bin/bash install.sh
