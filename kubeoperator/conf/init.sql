a2f80c6f259c234e2b48065a99f7b3c10915ca6b
xiaohu <xiaohu.lv@fit2cloud.com>
2020-07-30 15:42:41 +0800

    update mysql init

--- kubeoperator/conf/init.sql
+++ kubeoperator/conf/init.sql
@@ -1,2 +1,5 @@
 CREATE DATABASE `ko` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 CREATE DATABASE `grafana` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+use mysql;
+update user set host = '%' where user ='root';
+FLUSH PRIVILEGES;
