c361197d91f18b2544928b454a965bd98104debd
xiaohu <xiaohu.lv@fit2cloud.com>
2020-07-30 14:58:35 +0800

    update

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ if [ ! $CURRENT_DIR ];then
   CURRENT_DIR=$(cd "$(dirname "$0")";pwd)
 fi
 
-if read -t 120 -p "设置KubeOperator安装目录,默认/opt : " KO_BASE;then
+if read -t 180 -p "设置KubeOperator安装目录,默认/opt : " KO_BASE;then
   if [ "$KO_BASE" != "" ];then
     echo "你选择的安装路径为 $KO_BASE"
   else
@@ -113,14 +113,20 @@ fi
 #  3.启动 kubeoperator
 log "开始启动 KubeOperator"
 cd  $KO_BASE/kubeoperator/ && docker-compose up -d 2>&1 | tee -a ${CURRENT_DIR}/install.log
+sleep 15s
 while [ $(docker ps -a|grep kubeoperator |egrep "Exit|unhealthy"|wc -l) -gt 0 ]
 do
   for service in $(docker ps -a|grep kubeoperator |egrep "Exit|unhealthy"|awk '{print $1}')
   do
   docker restart ${service} 2>&1
   log "等待容器 ${service} 启动成功" | tee -a ${CURRENT_DIR}/install.log
-  sleep 5
+  sleep 5s
   done
 done
-echo -e "======================= KubeOperator 安装完成 =======================\n" 2>&1 | tee -a ${CURRENT_DIR}/install.log
-echo -e "请通过以下方式访问:\n URL: \033[33m http://$(hostname -I|cut -d" " -f 1)\033[0m \n 用户名: \033[${green}m admin \033[0m \n 初始密码: \033[${green}m kubeoperator@admin123 \033[0m" 2>&1 | tee -a ${CURRENT_DIR}/install.log
+if [ $(docker ps -a|grep kubeoperator |egrep "Exit|unhealthy"|wc -l) -eq 0 ];then
+  echo -e "======================= KubeOperator 安装完成 =======================\n" 2>&1 | tee -a ${CURRENT_DIR}/install.log
+  echo -e "请通过以下方式访问:\n URL: \033[33m http://$(hostname -I|cut -d" " -f 1)\033[0m \n 用户名: \033[${green}m admin \033[0m \n 初始密码: \033[${green}m kubeoperator@admin123 \033[0m" 2>&1 | tee -a ${CURRENT_DIR}/install.log
+else
+  echo -e "KubeOperator 服务异常，请检查服务是否启动" 2>&1 | tee -a ${CURRENT_DIR}/install.log
+  cd  $KO_BASE/kubeoperator/ && docker-compose status
+fi
