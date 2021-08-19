1069aa3ab69924ca97d863572d42a1dae66c8a10
wanghe-fit2cloud <wanghe@fit2cloud.com>
2021-08-19 12:10:55 +0800

    修复升级脚本

--- kubeoperator/scripts/const.sh
+++ kubeoperator/scripts/const.sh
@@ -13,6 +13,10 @@ export COMPOSE_HTTP_TIMEOUT=180
 export CURRENT_KO_VERSION=`cat ${KO_BASE}/kubeoperator/kubeoperator.conf|grep KO_TAG|awk -F= '{print $2}'`
 export OFFLINE_KO_VERSION=$(pwd|grep -Eo "v([0-9]{1,}\.)+[0-9]{1,}")
 export KO_PORT=`cat ${KO_BASE}/kubeoperator/kubeoperator.conf |grep KO_PORT|awk -F= '{print $2}'`
+export OS_ARCH=`cat ${KO_BASE}/kubeoperator/kubeoperator.conf |grep OS_ARCH|awk -F= '{print $2}'`
+export KO_REPO_PORT=`cat ${KO_BASE}/kubeoperator/kubeoperator.conf |grep KO_REPO_PORT|awk -F= '{print $2}'`
+export KO_REGISTRY_PORT=`cat ${KO_BASE}/kubeoperator/kubeoperator.conf |grep KO_REGISTRY_PORT|awk -F= '{print $2}'`
+export KO_REGISTRY_HOSTED_PORT=`cat ${KO_BASE}/kubeoperator/kubeoperator.conf |grep KO_REGISTRY_HOSTED_PORT|awk -F= '{print $2}'`
 
 function colorMsg() {
   echo -e "\033[$1m $2 \033[0m"
--- kubeoperator/scripts/upgrade.sh
+++ kubeoperator/scripts/upgrade.sh
@@ -14,26 +14,31 @@ function online_upgrade() {
     export KO_NEXUS_URL="https://kubeoperator.fit2cloud.com/nexus/nexus-${LATEST_KO_VERSION}.tar.gz"
 
     colorMsg $yellow "\n 提示:在线升级请确保当前主机可以正常连接互联网,升级前会先停止KubeOperator服务" | tee -a ${CWD}/upgrade.log
-    if read -p  "升级操作将会升级到目标版本: -$LATEST_KO_VERSION,是否继续 [y/n]: " yn;then
+    if read -p  "升级操作将会升级到目标版本: $LATEST_KO_VERSION,是否继续 [y/n]: " yn;then
        if [ "$yn" == "Y" ] || [ "$yn" == "y" ] || [ "$target" == "-y" ];then
           colorMsg $green "... 停止KubeOperator服务" | tee -a ${CWD}/upgrade.log
           koctl stop
-          sed -i -e "s#KO_TAG=.*#KO_TAG=$LATEST_KO_VERSION#g" $KO_BASE/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
+
           dir_name="${CWD}/kubeoperator-upgrade-$LATEST_KO_VERSION"
           rm -rf $dir_name | tee -a ${CWD}/upgrade.log
           mkdir -p $dir_name | tee -a ${CWD}/upgrade.log
+
           # download
           wget --no-check-certificate "${KO_INSTALL_URL}" -P $dir_name | tee -a ${CWD}/upgrade.log
           wget --no-check-certificate "${KO_ANSIBLE_URL}" -P $dir_name| tee -a ${CWD}/upgrade.log
           wget --no-check-certificate "${KO_NEXUS_URL}" -P $dir_name| tee -a ${CWD}/upgrade.log
+
           # untar
           tar zxf $dir_name/installer-${LATEST_KO_VERSION}.tar.gz -C $dir_name | tee -a ${CWD}/upgrade.log
           rm -rf  $KO_BASE/kubeoperator/data/nexus-data
           tar zxf $dir_name/nexus-${LATEST_KO_VERSION}.tar.gz -C $KO_BASE/kubeoperator/data | tee -a ${CWD}/upgrade.log
           tar zxf $dir_name/ansible-${LATEST_KO_VERSION}.tar.gz -C $dir_name  | tee -a ${CWD}/upgrade.log
           sed -i -e "1,9s#KO_BASE=.*#KO_BASE=${KO_BASE}#g" $dir_name/installer/koctl | tee -a ${CWD}/upgrade.log
+          \cp -rf $dir_name/installer/koctl /usr/local/bin | tee -a ${CWD}/upgrade.log
+
           # copy
           \cp -rf $dir_name/ansible/* $KO_BASE/kubeoperator/data/kobe/project/ko | tee -a ${CWD}/upgrade.log
+
           # 删除老版本遗留文件
           if [[ -d $KO_BASE/kubeoperator/conf/my.cnf ]]; then
             rm -rf $KO_BASE/kubeoperator/conf/my.cnf | tee -a ${CWD}/upgrade.log
@@ -42,11 +47,21 @@ function online_upgrade() {
           \cp -rf $dir_name/installer/kubeoperator/conf/* $KO_BASE/kubeoperator/conf/ | tee -a ${CWD}/upgrade.log
           \cp -rf $dir_name/installer/kubeoperator/docker-compose.yml $KO_BASE/kubeoperator/ | tee -a ${CWD}/upgrade.log
           \cp -rf $dir_name/installer/koctl $KO_BASE/kubeoperator/ | tee -a ${CWD}/upgrade.log
-           # 更新kubeoperator.conf 配置文件
-          sed -i -e "s#KO_TAG=.*#KO_TAG=$OFFLINE_KO_VERSION#g" $dir_name/installer/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
+
+          # 更新kubeoperator.conf 配置文件
+          sed -i -e "s#KO_TAG=.*#KO_TAG=$LATEST_KO_VERSION#g" $dir_name/installer/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
           sed -i -e "s#KO_BASE=.*#KO_BASE=$KO_BASE#g" $dir_name/installer/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
           sed -i -e "s#KO_PORT=.*#KO_PORT=$KO_PORT#g" $dir_name/installer/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
+          sed -i -e "s#OS_ARCH=.*#OS_ARCH=$OS_ARCH#g" $dir_name/installer/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
+          if [ $(grep -c KO_REPO_PORT $KO_BASE/kubeoperator/kubeoperator.conf) -eq '1' ];then
+            sed -i -e "s#KO_REPO_PORT=.*#KO_REPO_PORT=$KO_REPO_PORT#g" $dir_name/installer/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
+            sed -i -e "s#KO_REGISTRY_PORT=.*#KO_REGISTRY_PORT=$KO_REGISTRY_PORT#g" $dir_name/installer/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
+            sed -i -e "s#KO_REGISTRY_HOSTED_PORT=.*#KO_REGISTRY_HOSTED_PORT=$KO_REGISTRY_HOSTED_PORT#g" $dir_name/installer/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
+          fi
           \cp -rf $dir_name/installer/kubeoperator/kubeoperator.conf ${KO_BASE}/kubeoperator/kubeoperator.conf
+          sed -i -e "1,7s#KO_BASE=.*#KO_BASE=$KO_BASE#g" $dir_name/installer/kubeoperator/scripts/const.sh | tee -a ${CWD}/upgrade.log
+          \cp -rf $dir_name/installer/kubeoperator/scripts/* ${KO_BASE}/kubeoperator/scripts/
+
           colorMsg $yellow " ... 拉取镜像" | tee -a ${CWD}/upgrade.log
           ${KO_BASE}/kubeoperator/koctl pull | tee -a ${CWD}/upgrade.log
           ${KO_BASE}/kubeoperator/koctl start | tee -a ${CWD}/upgrade.log
@@ -73,12 +88,14 @@ function offline_upgrade() {
          [[ -e "$i" ]] || break
         docker load -i $i 2>&1 | tee -a ${CWD}/upgrade.log
      done
+
      colorMsg $green "... 解压离线包" | tee -a ${CWD}/upgrade.log
      sed -i -e "1,9s#KO_BASE=.*#KO_BASE=${KO_BASE}#g" ${CWD}/koctl
      \cp -rf ${CWD}/koctl /usr/local/bin | tee -a ${CWD}/upgrade.log
      rm -rf  $KO_BASE/kubeoperator/data/nexus-data
      tar zxf ${CWD}/nexus-data.tar.gz -C $KO_BASE/kubeoperator/data | tee -a ${CWD}/upgrade.log
      rm -rf $KO_BASE/kubeoperator/data/kobe/project/ko/*
+
      # 删除老版本遗留文件
      if [[ -d $KO_BASE/kubeoperator/conf/my.cnf ]]; then
        rm -rf $KO_BASE/kubeoperator/conf/my.cnf
@@ -87,11 +104,21 @@ function offline_upgrade() {
      \cp -rf ${CWD}/kubeoperator/conf/* $KO_BASE/kubeoperator/conf/ | tee -a ${CWD}/upgrade.log
      \cp -rf ${CWD}/kubeoperator/docker-compose.yml $KO_BASE/kubeoperator | tee -a ${CWD}/upgrade.log
      \cp -rf ${CWD}/kubeoperator/data/kobe/project/ko/* $KO_BASE/kubeoperator/data/kobe/project/ko
+
      # 更新kubeoperator.conf 配置文件
      sed -i -e "s#KO_TAG=.*#KO_TAG=$OFFLINE_KO_VERSION#g" ${CWD}/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
      sed -i -e "s#KO_BASE=.*#KO_BASE=$KO_BASE#g" ${CWD}/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
      sed -i -e "s#KO_PORT=.*#KO_PORT=$KO_PORT#g" ${CWD}/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
+     sed -i -e "s#OS_ARCH=.*#OS_ARCH=$OS_ARCH#g" ${CWD}/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
+     if [ $(grep -c KO_REPO_PORT $KO_BASE/kubeoperator/kubeoperator.conf) -eq '1' ];then
+      sed -i -e "s#KO_REPO_PORT=.*#KO_REPO_PORT=$KO_REPO_PORT#g" ${CWD}/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
+      sed -i -e "s#KO_REGISTRY_PORT=.*#KO_REGISTRY_PORT=$KO_REGISTRY_PORT#g" ${CWD}/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
+      sed -i -e "s#KO_REGISTRY_HOSTED_PORT=.*#KO_REGISTRY_HOSTED_PORT=$KO_REGISTRY_HOSTED_PORT#g" ${CWD}/kubeoperator/kubeoperator.conf | tee -a ${CWD}/upgrade.log
+     fi
      \cp -rf ${CWD}/kubeoperator/kubeoperator.conf ${KO_BASE}/kubeoperator/kubeoperator.conf
+     sed -i -e "1,7s#KO_BASE=.*#KO_BASE=$KO_BASE#g" ${CWD}/kubeoperator/scripts/const.sh | tee -a ${CWD}/upgrade.log
+     \cp -rf ${CWD}/kubeoperator/scripts/* ${KO_BASE}/kubeoperator/scripts/
+
      ${CWD}/koctl start
      colorMsg $green "升级完成，当前版本: $OFFLINE_KO_VERSION" | tee -a ${CWD}/upgrade.log
 }
@@ -99,9 +126,6 @@ function offline_upgrade() {
 function upgrade_init() {
     if [[ ! -d ${KO_BASE}/kubeoperator/scripts ]];then
       mkdir -p ${KO_BASE}/kubeoperator/scripts
-    fi
-    if [[ ! -f ${KO_BASE}/kubeoperator/scripts/const.sh ]];then
-      \cp -rp ${PROJECT_DIR}/* ${KO_BASE}/kubeoperator/scripts/
     fi
 }
 
